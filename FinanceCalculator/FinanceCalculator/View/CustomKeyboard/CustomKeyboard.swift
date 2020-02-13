c6af9051fa6fcf7b3d1abd1a2eef25a0abbff0fe
Dilum De Silva <dilumdesilva.info@gmail.com>
2020-02-14 03:13:56 +0530

    Added: reusable views - custom neumaric keybaord

---
+++ FinanceCalculator/FinanceCalculator/View/CustomKeyboard/CustomKeyboard.swift
@@ -0,0 +1,29 @@
+//
+//  CustomKeyboard.swift
+//  FinanceCalculator
+//
+//  Created by Dilum De Silva on 2/14/20.
+//  Copyright © 2020 Dilum De Silva. All rights reserved.
+//
+
+import UIKit
+
+//constant which holds the defualt keyboard height
+
+//constants that hold button colors
+
+class CustomKeyboard: UIView {
+
+    //Outlet connections related the custom keyboard UI elements
+    
+    //Number pad buttons to change the apperance
+    
+    //Keyboard - vController connection variables
+    
+    //Vairables that set the appearence
+    
+    //Initialization of the custom keyboard
+    
+    //Functions to handle button clicks
+    
+}
