27f903916f1d229fba111bce63f9d468e98e3cb9
Dilum De Silva <dilumdesilva.info@gmail.com>
2020-03-09 02:05:41 +0530

    Dev: Mortgage and Loan views done

--- FinanceCalculator/FinanceCalculator/Controller/CalculationsViewController.swift
+++ FinanceCalculator/FinanceCalculator/Controller/CalculationsViewController.swift
@@ -8,24 +8,52 @@
 
 import UIKit
 
-class CalculationsViewController: UIViewController{
-    
-    @IBOutlet weak var calculationsCollectionView: UICollectionView!
-    
+class CalculationsViewController: UIViewController {
+    @IBOutlet var calculationsCollectionView: UICollectionView!
     
     var calculations = [Calculation]()
     
+    var estimateWidth = 160.0
+    var cellMarginSize = 18.0
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        //setCustomNavigationBar
+        setCustomNavBar()
         
-        //set Delegates
-        //self.calculationsCollectionView.delegate = self
+        super.viewDidLoad()
+
+        // set Delegates
+        self.calculationsCollectionView.delegate = self
         self.calculationsCollectionView.dataSource = self
         
-        //Register Cells
+        // Register Cells
         self.calculationsCollectionView.register(UINib(nibName: "CardCell", bundle: nil), forCellWithReuseIdentifier: "CardCell")
         
-        generateCalculations()
+        // Setup GridView
+        self.setupGridView()
+        
+        //Generate Calculations
+        self.generateCalculations()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        self.setupGridView()
+        DispatchQueue.main.async {
+            self.calculationsCollectionView.reloadData()
+        }
+    }
+    
+    func setCustomNavBar(){
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: nil, action: nil)
+    }
+    
+    
+    func setupGridView() {
+        let flow = calculationsCollectionView?.collectionViewLayout as! UICollectionViewFlowLayout
+        flow.minimumInteritemSpacing = CGFloat(self.cellMarginSize)
+        flow.minimumLineSpacing = CGFloat(self.cellMarginSize)
     }
     
     /// This function generates calculation types
@@ -35,51 +63,64 @@ class CalculationsViewController: UIViewController{
     ///     - Compound Savings
     ///     - Loans
     ///     - Mortgage
-    func generateCalculations(){
-        let compundSavings = Calculation(name: "Savings", icon: UIImage(named: "icon_savings")!, segueID: "goToSavingsCalculation", cellColour: UIColor(red: 30/255, green: 30/255, blue: 30/255, alpha: 1.00))
-        let loans = Calculation(name: "Loans", icon: UIImage(named: "icon_loan")!, segueID: "goToLoansCalculation", cellColour: UIColor(red: 30/255, green: 30/255, blue: 30/255, alpha: 1.00))
-        let mortgage = Calculation(name: "Mortgage", icon: UIImage(named: "icon_mortgage")!, segueID: "goToMortgageCalculation", cellColour: UIColor(red: 30/255, green: 30/255, blue: 30/255, alpha: 1.00))
+    func generateCalculations() {
+        let compundSavings = Calculation(name: "Savings", icon: UIImage(named: "icon_savings")!, segueID: "goToSavingsCalculation", cellColour: UIColor(red: 230 / 255, green: 230 / 255, blue: 230 / 255, alpha: 1.00))
+        let loans = Calculation(name: "Loan", icon: UIImage(named: "icon_loan")!, segueID: "goToLoansCalculation", cellColour: UIColor(red: 230 / 255, green: 230 / 255, blue: 230 / 255, alpha: 1.00))
+        let mortgage = Calculation(name: "Mortgage", icon: UIImage(named: "icon_mortgage")!, segueID: "goToMortgageCalculation", cellColour: UIColor(red: 230 / 255, green: 230 / 255, blue: 230 / 255, alpha: 1.00))
         
         calculations += [compundSavings, loans, mortgage]
     }
 }
 
+// MARK: - Extenstions of protocol stubs related to the calculations collection view
 
-extension CalculationsViewController: UICollectionViewDataSource{
-    //MARK: Protocol Stubs related to the calculations collection view
-    
-    ///Return - calculation count to be used to populate the collection view
+extension CalculationsViewController: UICollectionViewDataSource {
+    /// Return - calculation count to be used to populate the collection view
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return calculations.count
+        return self.calculations.count
     }
     
-    ///Generate - calculation collection view cell
+    /// Generate - calculation collection view cell
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         /// Initializing a reusable sell
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath) as! CardCell
         
         /// Initializing cell content and assigning data source
         cell.setData(text: self.calculations[indexPath.row].getCalculationName(), icon: self.calculations[indexPath.row].getCalculationIcon())
-
         
         /// Initializing cell styles
-        cell.contentView.backgroundColor = calculations[indexPath.row].getCellColour()
-        cell.contentView.layer.cornerRadius = 10.0
-        cell.contentView.layer.borderWidth = 1.0
-        cell.contentView.layer.borderColor = UIColor(red: 0.25, green: 0.25, blue: 0.25, alpha: 1.00).cgColor
+        cell.contentView.backgroundColor = self.calculations[indexPath.row].getCellColour()
+        cell.contentView.layer.cornerRadius = 15.0
+        cell.contentView.layer.borderWidth = 2.0
+        cell.contentView.layer.borderColor = UIColor(red: 0.86, green: 0.86, blue: 0.86, alpha: 1.00).cgColor
         cell.contentView.layer.masksToBounds = false
         
         return cell
     }
     
-    ///Function to be invoked when the user clicks an item in the collection view
+    /// Function to be invoked when the user clicks an item in the collection view
     ///
-    ///Usage:
+    /// Usage:
     ///     performSegue() - this function has been used to navigate to the relevant page using the segueID
     ///         params:  Identifier.SegueID
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        performSegue(withIdentifier: calculations[indexPath.row].getSegueID(), sender: self)
+        performSegue(withIdentifier: self.calculations[indexPath.row].getSegueID(), sender: self)
     }
 }
 
-
+extension CalculationsViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = self.calculateWidth()
+        return CGSize(width: width, height: width)
+    }
+    
+    func calculateWidth() -> CGFloat {
+        let estimatedWidth = CGFloat(estimateWidth)
+        let cellCount = floor(CGFloat(self.view.frame.size.width / estimatedWidth))
+        
+        let margin = CGFloat(cellMarginSize * 2)
+        let width = (self.view.frame.size.width - CGFloat(self.cellMarginSize) * (cellCount - 1) - margin) / cellCount
+        
+        return width
+    }
+}
