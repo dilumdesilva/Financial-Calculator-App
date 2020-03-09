53c2d5207a7482f39f6bc0aac668a68ba31e7d03
Dilum De Silva <dilumdesilva.info@gmail.com>
2020-03-09 15:38:34 +0530

    Impd: UITextField+CustomKeyboard extension implemented

--- FinanceCalculator/FinanceCalculator/Helpers/Extensions/UITextField+CustomKeyboard.swift
+++ FinanceCalculator/FinanceCalculator/Helpers/Extensions/UITextField+CustomKeyboard.swift
@@ -6,4 +6,45 @@
 //  Copyright © 2020 Dilum De Silva. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+private var numericKeyboardDelegate: CustomKeyboardDelegate? = nil
+
+extension UITextField: CustomKeyboardDelegate{
+    
+    func setAsNumericKeyboard(delegate: CustomKeyboardDelegate?) {
+        let numericKeyboard = CustomKeyboard(frame: CGRect (x: 0, y: 0, width: 0, height: customKeyboardHeight))
+        self.inputView = numericKeyboard
+        numericKeyboardDelegate = delegate
+        numericKeyboard.delegate = self
+    }
+    
+    func unsetAsNumericKeyboard(){
+        if let numericKeyboard = self.inputView as? CustomKeyboard {
+            numericKeyboard.delegate = nil
+        }
+        self.inputView = nil
+        numericKeyboardDelegate = nil
+    }
+    
+    internal func numericKeyPressed(key: Int) {
+        self.insertText(String(key))
+        numericKeyboardDelegate?.numericKeyPressed(key: key)
+    }
+    
+    internal func backspacePressed() {
+        self.deleteBackward()
+        numericKeyboardDelegate?.backspacePressed()
+    }
+    
+    internal func symbolPressed(symbol: String) {
+        self.insertText(String(symbol))
+        numericKeyboardDelegate?.symbolPressed(symbol: symbol)
+    }
+    
+    internal func hideKeyboardPressed() {
+        numericKeyboardDelegate?.hideKeyboardPressed()
+    }
+    
+    
+}
