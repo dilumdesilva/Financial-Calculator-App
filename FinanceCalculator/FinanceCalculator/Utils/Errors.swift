c3314451b2b9c50e0790121dc34e18d1c5023813
Dilum De Silva <dilumdesilva.info@gmail.com>
2020-03-10 08:32:33 +0530

    Added: Util for errors related to the calculations

---
+++ FinanceCalculator/FinanceCalculator/Utils/Errors.swift
@@ -0,0 +1,17 @@
+//
+//  Errors.swift
+//  FinanceCalculator
+//
+//  Created by Dilum De Silva on 3/10/20.
+//  Copyright © 2020 Dilum De Silva. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    /// To indicate runtime errors during the calculations
+    enum CalculationError: Error {
+        case runtimeError(String)
+    }
+}
