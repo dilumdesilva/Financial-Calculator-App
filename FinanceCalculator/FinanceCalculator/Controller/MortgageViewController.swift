0d04dc3fe6965fe9afdf5f5f6ab19cf4c10b7de2
Dilum De Silva <dilumdesilva.info@gmail.com>
2020-03-10 11:49:42 +0530

    Impd: Mortgage calculation completed

--- FinanceCalculator/FinanceCalculator/Controller/MortgageViewController.swift
+++ FinanceCalculator/FinanceCalculator/Controller/MortgageViewController.swift
@@ -9,47 +9,46 @@
 import UIKit
 
 let MORTGAGE_USER_DEFAULTS_KEY = "mortgage"
-private let MORTGAGE_USER_DEFAULTS_MAX_COUNT = 5
+private let MORTGAGE_USER_DEFAULTS_MAX_COUNT = 3
 
 class MortgageViewController: UIViewController, CustomKeyboardDelegate {
-
-    @IBOutlet weak var viewScroller: UIScrollView!
-    @IBOutlet weak var outerStackView: UIStackView!
-    @IBOutlet weak var outerStackViewTopConstaint: NSLayoutConstraint!
-    @IBOutlet weak var mortgageLoanAmountTFStackView: UIStackView!
-    @IBOutlet weak var loanAmountTextField: UITextField!
-    @IBOutlet weak var mortgageInterestTFStackView: UIStackView!
-    @IBOutlet weak var interestTextField: UITextField!
-    @IBOutlet weak var mortgagePaymentTFStackView: UIStackView!
-    @IBOutlet weak var paymentTextField: UITextField!
-    @IBOutlet weak var mortgageNoYearsTFStackView: UIStackView!
-    @IBOutlet weak var numberOfYearsTextField: UITextField!
+    @IBOutlet var viewScroller: UIScrollView!
+    @IBOutlet var outerStackView: UIStackView!
+    @IBOutlet var outerStackViewTopConstaint: NSLayoutConstraint!
+    @IBOutlet var mortgageLoanAmountTFStackView: UIStackView!
+    @IBOutlet var loanAmountTextField: UITextField!
+    @IBOutlet var mortgageInterestTFStackView: UIStackView!
+    @IBOutlet var interestTextField: UITextField!
+    @IBOutlet var mortgagePaymentTFStackView: UIStackView!
+    @IBOutlet var paymentTextField: UITextField!
+    @IBOutlet var mortgageNoYearsTFStackView: UIStackView!
+    @IBOutlet var numberOfTermsInMonths: UITextField!
     
-    @IBOutlet weak var btnCalculate: UIBarButtonItem!
-    @IBOutlet weak var btnReset: UIBarButtonItem!
-    @IBOutlet weak var btnSave: UIBarButtonItem!
+    @IBOutlet var btnCalculate: UIBarButtonItem!
+    @IBOutlet var btnReset: UIBarButtonItem!
+    @IBOutlet var btnSave: UIBarButtonItem!
     
     var activeTextField = UITextField()
     var outerStackViewTopConstraintDefaultHeight: CGFloat = 10.0
     var textFieldToKeyBoardGap = 10
-    var keyBoardHeight:CGFloat = 0
+    var keyBoardHeight: CGFloat = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(keyboardWillHide)))
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(keyboardWillHide)))
         
         if validateTexFields() == 0 {
-            self.btnReset.isEnabled = false
-            self.btnCalculate.isEnabled = false
-            self.btnSave.isEnabled = false
+            btnReset.isEnabled = false
+            btnCalculate.isEnabled = false
+            btnSave.isEnabled = false
         }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        ///setting text field styles
+        /// setting text field styles
         loanAmountTextField._darkPlaceHolderColor(UIColor.darkText)
         loanAmountTextField.setAsNumericKeyboard(delegate: self)
         
@@ -59,16 +58,16 @@ class MortgageViewController: UIViewController, CustomKeyboardDelegate {
         paymentTextField._darkPlaceHolderColor(UIColor.darkText)
         paymentTextField.setAsNumericKeyboard(delegate: self)
         
-        numberOfYearsTextField._darkPlaceHolderColor(UIColor.darkText)
-        numberOfYearsTextField.setAsNumericKeyboard(delegate: self)
+        numberOfTermsInMonths._darkPlaceHolderColor(UIColor.darkText)
+        numberOfTermsInMonths.setAsNumericKeyboard(delegate: self)
         
-        ///Obser which tracks the keyboard show event
+        /// Obser which tracks the keyboard show event
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)),
                                                name: UIResponder.keyboardWillShowNotification, object: nil)
     }
     
     /// This will function will invoked by the ui tap gesture
-    @objc func keyboardWillHide(){
+    @objc func keyboardWillHide() {
         view.endEditing(true)
         
         UIView.animate(withDuration: 0.25, animations: {
@@ -77,23 +76,23 @@ class MortgageViewController: UIViewController, CustomKeyboardDelegate {
         })
     }
     
-    @objc func keyboardWillShow(notification: NSNotification){
-        let firstResponder = self.findFirstResponder(inView: self.view)
+    @objc func keyboardWillShow(notification: NSNotification) {
+        let firstResponder = findFirstResponder(inView: view)
         
-        if firstResponder != nil{
-            activeTextField = firstResponder as! UITextField;
+        if firstResponder != nil {
+            activeTextField = firstResponder as! UITextField
             
             let activeTextFieldSuperView = activeTextField.superview!
             
-            if let info = notification.userInfo{
-                let keyboard:CGRect = info["UIKeyboardFrameEndUserInfoKey"] as! CGRect
+            if let info = notification.userInfo {
+                let keyboard: CGRect = info["UIKeyboardFrameEndUserInfoKey"] as! CGRect
                 let targetY = view.frame.size.height - keyboard.height - 15 - activeTextField.frame.size.height
                 let initialY = outerStackView.frame.origin.y + activeTextFieldSuperView.frame.origin.y + activeTextField.frame.origin.y
-
+                
                 if initialY > targetY {
                     let diff = targetY - initialY
                     let targetOffsetForTopConstraint = outerStackViewTopConstaint.constant + diff
-                    self.view.layoutIfNeeded()
+                    view.layoutIfNeeded()
                     
                     UIView.animate(withDuration: 0.25, animations: {
                         self.outerStackViewTopConstaint.constant = targetOffsetForTopConstraint
@@ -101,7 +100,7 @@ class MortgageViewController: UIViewController, CustomKeyboardDelegate {
                     })
                 }
                 
-                var contentInset:UIEdgeInsets = self.viewScroller.contentInset
+                var contentInset: UIEdgeInsets = viewScroller.contentInset
                 contentInset.bottom = keyboard.size.height
                 viewScroller.contentInset = contentInset
             }
@@ -122,54 +121,101 @@ class MortgageViewController: UIViewController, CustomKeyboardDelegate {
         return nil
     }
     
-    
     @IBAction func performCalculation(_ sender: Any) {
         if validateTexFields() == 3 {
-            showAlert(message: "can perform", title: "performCalculation")
-            
             /// calculate Missing Component
             calculateMissingComponent()
+            btnSave.isEnabled = true
             
-        }else if validateTexFields() == 4 {
+        } else if validateTexFields() == 4 {
             showAlert(message: "Invalid Calculation. You must leave one field to proceed with a calculation", title: "Mortgage Calculation Warning")
-        }else {
+        } else {
             showAlert(message: "Please fill at least three fields to perform a calculation", title: "Mortgage Calculation Warning")
         }
     }
     
-    ///Function which is getting triggered once a textbox is changed
+    /// This function will trigger when user pressed on the save button
+    @IBAction func saveCalculation(_ sender: UIBarButtonItem) {
+        
+        ///
+        /// P           -    loanAmount
+        /// R           -    interest rate
+        /// PMT      -    Payment
+        /// N           -    Number of Terms in months
+        ///
+        if !isTextFieldEmpty(){
+            let calculation = "\(loanAmountTextField.text) P = \(interestTextField.text) R = \(paymentTextField.text) PMT = \(numberOfTermsInMonths) N ="
+            
+            var arr = UserDefaults.standard.array(forKey: MORTGAGE_USER_DEFAULTS_KEY) as? [String] ?? []
+            
+            if arr.count >= MORTGAGE_USER_DEFAULTS_MAX_COUNT{
+                arr = Array(arr.suffix(MORTGAGE_USER_DEFAULTS_MAX_COUNT - 1))
+            }
+            
+            arr.append(calculation)
+            UserDefaults.standard.set(arr, forKey: MORTGAGE_USER_DEFAULTS_KEY)
+            
+            showAlert(message: "You Mortgage Calculation has been saved successfully", title: "Saving Successfull")
+            
+        }else{
+            showAlert(message: "You are trying to save an empty conversion!", title: "Moartage Calculation Saving Error")
+        }
+    }
+    
+    
+    /// Function which is getting triggered once a textbox is changed
     @IBAction func mortgageTextFieldDidChange(_ sender: UITextField) {
-        self.btnReset.isEnabled = true
-        self.btnCalculate.isEnabled = true
+        btnReset.isEnabled = true
+        btnCalculate.isEnabled = true
     }
     
     @IBAction func resetMortageView(_ sender: Any) {
         resetTextFields()
     }
     
-    func calculateMissingComponent(){
+    func calculateMissingComponent() {
+        ///
+        /// A           -    the future value of the investment/loan, including interest
+        /// P           -    the principal investment amount (the initial deposit or loan amount – present value)
+        /// R           -    the annual interest rate (e.g. 3.2% is 0.032)
+        /// N           -    the number of times that interest is compounded per unit time (this is always monthly for the purpose of this coursework, i.e. 12 per year)
+        /// T           -     the time the money is invested or borrowed in months
+        /// PMT      -    the Payment
+        /// PayPY   -    the number of payments per year
+        /// CpY       -    the number of compound payments per year
+        /// PmtAt    -    the payment due at the beginning or end of each period (default is END)
+        ///
+        let P = Double(loanAmountTextField.text!)
+        let R = Double(interestTextField.text!)
+        let PMT = Double(paymentTextField.text!)
+        let N = Double(numberOfTermsInMonths.text!)
+        
         /// Identify missing component
         var missingValue = 0.0
         if (loanAmountTextField.text?.isEmpty)! {
-            missingValue = missingPrincipalAmount()
+            missingValue = missingPrincipalAmount(interest: R!, monthlyPayment: PMT!, terms: N!)
             loanAmountTextField.text = String(missingValue)
         }
         if (interestTextField.text?.isEmpty)! {
-            missingValue = missingInterestRate()
+            missingValue = missingInterestRate(principalAmount: P!, monthlyPayment: PMT!, terms: N!)
             interestTextField.text = String(missingValue)
         }
         if (paymentTextField.text?.isEmpty)! {
-            missingValue = missingMonthlyPayment()
+            missingValue = missingMonthlyPayment(interest: R!, principalAmount: P!, terms: N!)
             paymentTextField.text = String(missingValue)
         }
-        if (numberOfYearsTextField.text?.isEmpty)! {
-            missingValue = missingPaymentTerms()
-            numberOfYearsTextField.text = String(missingValue)
+        if (numberOfTermsInMonths.text?.isEmpty)! {
+            do {
+                try missingValue = Double(missingPaymentTerms(interest: R!, principalAmount: P!, monthlyPayment: PMT!))
+            } catch let err  {
+                print(err)
+            }
+            numberOfTermsInMonths.text = String(missingValue)
         }
         /// Perform relavant calculation
     }
     
-    func validateTexFields() -> Int{
+    func validateTexFields() -> Int {
         var counter = 0
         if !(loanAmountTextField.text?.isEmpty)! {
             counter += 1
@@ -180,25 +226,21 @@ class MortgageViewController: UIViewController, CustomKeyboardDelegate {
         if !(paymentTextField.text?.isEmpty)! {
             counter += 1
         }
-        if !(numberOfYearsTextField.text?.isEmpty)! {
+        if !(numberOfTermsInMonths.text?.isEmpty)! {
             counter += 1
         }
         
         return counter
     }
     
-    func updateTextFeilds(){
-        
-    }
-    
     /// Resetting the text feilds
     /// - Warning: This function needs to be changed when a new text field is added
     ///
-    func resetTextFields(){
-            loanAmountTextField.text = ""
-            interestTextField.text = ""
-            paymentTextField.text = ""
-            numberOfYearsTextField.text = ""
+    func resetTextFields() {
+        loanAmountTextField.text = ""
+        interestTextField.text = ""
+        paymentTextField.text = ""
+        numberOfTermsInMonths.text = ""
     }
     
     /// Resuable function to check whether the text feilds are empty
@@ -207,13 +249,12 @@ class MortgageViewController: UIViewController, CustomKeyboardDelegate {
     /// Usage:
     ///     if isTextFieldsEmpty() // true | false
     ///
-    func isTextFieldEmpty() -> Bool{
-        if !(loanAmountTextField.text?.isEmpty)! && !(interestTextField.text?.isEmpty)! && !(paymentTextField.text?.isEmpty)! && !(numberOfYearsTextField.text?.isEmpty)!{
+    func isTextFieldEmpty() -> Bool {
+        if !(loanAmountTextField.text?.isEmpty)!, !(interestTextField.text?.isEmpty)!, !(paymentTextField.text?.isEmpty)!, !(numberOfTermsInMonths.text?.isEmpty)! {
             return false
         }
         return true
     }
-    
     
     func numericKeyPressed(key: Int) {
         print("Numeric key \(key) pressed!")
@@ -221,7 +262,6 @@ class MortgageViewController: UIViewController, CustomKeyboardDelegate {
     
     func backspacePressed() {
         print("Backspace pressed!")
-        
     }
     
     func symbolPressed(symbol: String) {
@@ -231,5 +271,4 @@ class MortgageViewController: UIViewController, CustomKeyboardDelegate {
     func hideKeyboardPressed() {
         keyboardWillHide()
     }
-    
 }
